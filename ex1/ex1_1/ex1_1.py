b1b467335e405557192fd01d5f5b87320ea950b7
e195718 <e195718@ie.u-ryukyu.ac.jp>
2021-10-05 20:38:03 +0900

    first commit

---
+++ ex1/ex1_1/ex1_1.py
@@ -0,0 +1,28 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+def true_function(x):
+    '''
+    >>> true_function(0)
+    0.0
+    '''
+    y = np.sin(np.pi * x * 0.8) * 10
+    return y
+
+fig = plt.figure(figsize=(4, 4))
+ax = fig.add_subplot(111)
+
+ax.set_xlabel("x", fontsize = 10)
+ax.set_ylabel("y", fontsize = 10)
+
+x = np.arange(-1, 1, np.pi/36)
+ax.plot(x, true_function(x), label = "y = sin(π*x*0.8)*10")
+
+ax.legend(loc = "lower right")
+
+plt.show()
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
