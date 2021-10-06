f1df1263123efdddb0fc348f8987ec8062fabb33
e195718 <e195718@ie.u-ryukyu.ac.jp>
2021-10-06 23:55:48 +0900

    first commit

---
+++ ex1/ex1_2/ex1_2.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.append("../ex1_1")
+import pandas as pd
+from ex1_1 import true_function
+import random
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+random.seed(0)
+
+cols = ['観測点', '真値']
+df = pd.DataFrame(index=[], columns=cols)
+
+for i in range(20):
+    x = random.uniform(-1,1)
+    y = true_function(x)
+    record = pd.Series([x,y], index=df.columns)
+    df = df.append(record, ignore_index=True)
+
+
+if __name__ == '__main__':
+    fig = plt.figure(figsize=(4, 4))
+    ax = fig.add_subplot(111)
+
+    ax.set_xlabel("x", fontsize = 10)
+    ax.set_ylabel("y", fontsize = 10)
+
+    x = np.arange(-1.0, 1.1, 0.1)
+    ax.plot(x, true_function(x), label = "y = sin(π*x*0.8)*10")
+    plt.scatter(df['観測点'], df['真値'], c='k', s=10)
+
+    ax.legend(loc = "lower right")
+
+    plt.show()
